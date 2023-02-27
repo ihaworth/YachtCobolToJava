f7991d128b136acfbf64f7148263abe3cf92c708
ian.haworth <ian.haworth@opencastsoftware.com>
2023-02-27 14:30:30 +0000

    Inline redundant variable in tests

--- test/YACHTTest.java
+++ test/YACHTTest.java
@@ -8,176 +8,147 @@ class YACHTTest {
 
     @Test
     void yacht() {
-        int result = yacht.calculate("55555", "yacht");
-        assertEquals(50, result);
+        assertEquals(50, yacht.calculate("55555", "yacht"));
     }
 
     @Test
     void Not_Yacht() {
-        int result = yacht.calculate("13325", "yacht");
-        assertEquals(0, result);
+        assertEquals(0, yacht.calculate("13325", "yacht"));
     }
 
     @Test
     void Ones() {
-        int result = yacht.calculate("11135", "ones");
-        assertEquals(3, result);
+        assertEquals(3, yacht.calculate("11135", "ones"));
     }
 
     @Test
     void Ones_out_of_order() {
-        int result = yacht.calculate("31151", "ones");
-        assertEquals(3, result);
+        assertEquals(3, yacht.calculate("31151", "ones"));
     }
 
     @Test
     void No_ones() {
-        int result = yacht.calculate("43655", "ones");
-        assertEquals(0, result);
+        assertEquals(0, yacht.calculate("43655", "ones"));
     }
 
     @Test
     void Twos() {
-        int result = yacht.calculate("23456", "twos");
-        assertEquals(2, result);
+        assertEquals(2, yacht.calculate("23456", "twos"));
     }
 
     @Test
     void Fours() {
-        int result = yacht.calculate("14141", "fours");
-        assertEquals(8, result);
+        assertEquals(8, yacht.calculate("14141", "fours"));
     }
 
     @Test
     void Yacht_counted_as_threes() {
-        int result = yacht.calculate("33333", "threes");
-        assertEquals(15, result);
+        assertEquals(15, yacht.calculate("33333", "threes"));
     }
 
     @Test
     void Yacht_of_3s_counted_as_fives() {
-        int result = yacht.calculate("33333", "fives");
-        assertEquals(0, result);
+        assertEquals(0, yacht.calculate("33333", "fives"));
     }
 
     @Test
     void Fives() {
-        int result = yacht.calculate("15353", "fives");
-        assertEquals(10, result);
+        assertEquals(10, yacht.calculate("15353", "fives"));
     }
 
     @Test
     void Sixes() {
-        int result = yacht.calculate("23456", "sixes");
-        assertEquals(6, result);
+        assertEquals(6, yacht.calculate("23456", "sixes"));
     }
 
     @Test
     void Full_house_two_small_three_big() {
-        int result = yacht.calculate("22444", "full house");
-        assertEquals(16, result);
+        assertEquals(16, yacht.calculate("22444", "full house"));
     }
 
     @Test
     void Full_house_three_small_two_big() {
-        int result = yacht.calculate("53353", "full house");
-        assertEquals(19, result);
+        assertEquals(19, yacht.calculate("53353", "full house"));
     }
 
     @Test
     void Two_pair_is_not_a_full_house() {
-        int result = yacht.calculate("22445", "full house");
-        assertEquals(0, result);
+        assertEquals(0, yacht.calculate("22445", "full house"));
     }
 
     @Test
     void Four_of_a_kind_is_not_a_full_house() {
-        int result = yacht.calculate("14444", "full house");
-        assertEquals(0, result);
+        assertEquals(0, yacht.calculate("14444", "full house"));
     }
 
     @Test
     void Yacht_is_not_a_full_house() {
-        int result = yacht.calculate("22222", "full house");
-        assertEquals(0, result);
+        assertEquals(0, yacht.calculate("22222", "full house"));
     }
 
     @Test
     void Four_of_a_Kind() {
-        int result = yacht.calculate("66466", "four of a kind");
-        assertEquals(24, result);
+        assertEquals(24, yacht.calculate("66466", "four of a kind"));
     }
 
     @Test
     void Yacht_can_be_scored_as_Four_of_a_Kind() {
-        int result = yacht.calculate("33333", "four of a kind");
-        assertEquals(12, result);
+        assertEquals(12, yacht.calculate("33333", "four of a kind"));
     }
 
     @Test
     void Full_house_is_not_Four_of_a_Kind() {
-        int result = yacht.calculate("33355", "four of a kind");
-        assertEquals(0, result);
+        assertEquals(0, yacht.calculate("33355", "four of a kind"));
     }
 
     @Test
     void Little_Straight() {
-        int result = yacht.calculate("35412", "little straight");
-        assertEquals(30, result);
+        assertEquals(30, yacht.calculate("35412", "little straight"));
     }
 
     @Test
     void Little_Straight_as_Big_Straight() {
-        int result = yacht.calculate("12345", "big straight");
-        assertEquals(0, result);
+        assertEquals(0, yacht.calculate("12345", "big straight"));
     }
 
     @Test
     void Four_in_order_but_not_a_little_straight() {
-        int result = yacht.calculate("11234", "little straight");
-        assertEquals(0, result);
+        assertEquals(0, yacht.calculate("11234", "little straight"));
     }
 
     @Test
     void No_pairs_but_not_a_little_straight() {
-        int result = yacht.calculate("12346", "little straight");
-        assertEquals(0, result);
+        assertEquals(0, yacht.calculate("12346", "little straight"));
     }
 
     @Test
     void Minimum_is_1_maximum_is_5_but_not_a_little_straight() {
-        int result = yacht.calculate("11345", "little straight");
-        assertEquals(0, result);
+        assertEquals(0, yacht.calculate("11345", "little straight"));
     }
 
     @Test
     void Big_Straight() {
-        int result = yacht.calculate("46253", "big straight");
-        assertEquals(30, result);
+        assertEquals(30, yacht.calculate("46253", "big straight"));
     }
 
     @Test
     void Big_Straight_as_little_straight() {
-        int result = yacht.calculate("65432", "little straight");
-        assertEquals(0, result);
+        assertEquals(0, yacht.calculate("65432", "little straight"));
     }
 
     @Test
     void No_pairs_but_not_a_big_straight() {
-        int result = yacht.calculate("65431", "big straight");
-        assertEquals(0, result);
+        assertEquals(0, yacht.calculate("65431", "big straight"));
     }
 
     @Test
     void Choice() {
-        int result = yacht.calculate("33566", "choice");
-        assertEquals(23, result);
+        assertEquals(23, yacht.calculate("33566", "choice"));
     }
 
     @Test
     void Yacht_as_choice() {
-        int result = yacht.calculate("22222", "choice");
-        assertEquals(10, result);
+        assertEquals(10, yacht.calculate("22222", "choice"));
     }
 
 }
